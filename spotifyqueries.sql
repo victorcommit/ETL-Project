3b79f00aa5bbef1b8948dfd6332f359890cb6378
victorcommit <alvinvictor@dyn-160-39-225-147.dyn.columbia.edu>
2020-01-25 13:04:09 -0500

    added resources information

---
+++ spotifyqueries.sql
@@ -0,0 +1,20 @@
+-- Create tables for raw data to be loaded into 
+CREATE TABLE spotify_list (
+    id INT PRIMARY KEY,
+    title TEXT,
+    artist TEXT,
+    top genre TEXT,
+    year DATE,
+    bpm INT ,
+    nrgy INT ,
+    dnce INT ,
+    dB INT ,
+    live INT ,
+    val INT ,
+    dur INT ,
+    acous INT ,
+    spch INT ,
+    pop INT ,
+    );
+
+CREATE TABLE 
